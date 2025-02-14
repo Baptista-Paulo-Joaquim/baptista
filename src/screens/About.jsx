01ed2dc6db8845753c0b0adec16a9325b96ffbc7
Baptista-Paulo-Joaquim <baptistajoaquim0@gmail.com>
2025-02-14 14:25:47 +0200

    Hight for all pages

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -1,12 +1,13 @@
 export default function About() {
     return (
-        <section className="bg-gray-900 text-white">
-            <div className="mx-auto max-w-screen-xl px-4 py-30 lg:flex lg:h-screen lg:items-center">
+        <section className="h-screen bg-gray-900 text-white">
+            <div className="mx-auto max-w-screen-xl px-4 py-30 flex h-full items-center">
                 <div className="mx-auto max-w-3xl text-center">
-                <h1 className="bg-gradient-to-r from-green-300 to-purple-600 bg-clip-text text-3xl  text-transparent sm:text-3xl"> About page! </h1>
+                    <h1 className="bg-gradient-to-r from-green-300 to-purple-600 bg-clip-text text-3xl text-transparent sm:text-3xl">
+                        About page!
+                    </h1>
                 </div>
             </div>
-            
         </section>
     );
-  }
+}
